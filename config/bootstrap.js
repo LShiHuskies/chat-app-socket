b5add5db6c2da5d4197f93cd16f944521a4745d4
Louis Shi <Shilou2010@gmail.com>
2019-04-11 10:12:42 -0400

    chatroom

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,7 +9,7 @@
  * https://sailsjs.com/config/bootstrap
  */
 
-module.exports.bootstrap = async function() {
+module.exports.bootstrap = async function(cb) {
   sails.config.appName = "New Sails App";
   // By convention, this is a good place to set up fake data during development.
   //
@@ -26,5 +26,44 @@ module.exports.bootstrap = async function() {
   //   // etc.
   // ]);
   // ```
+
+  // Generate Chat Messages
+try {
+  let messageCount = ChatMessage.count();
+  if(messageCount > 0){
+    return; // don't repeat messages
+  }
+
+  let users = await User.find();
+  if(users.length >= 3) {
+    console.log("Generating messages...")
+
+    let msg1 = await ChatMessage.create({
+      message: 'Hey Everyone! Welcome to the community!',
+      createdBy: users[1]
+    });
+    console.log("Created Chat Message: " + msg1.id);
+
+    let msg2 = await ChatMessage.create({
+      message: "How's it going?",
+      createdBy: users[2]
+    });
+    console.log("Created Chat Message: " + msg2.id);
+
+    let msg3 = await ChatMessage.create({
+      message: 'Super excited!',
+      createdBy: users[0]
+    });
+    console.log("Created Chat Message: " + msg3.id);
+
+  } else {
+    console.log('skipping message generation');
+  }
+}catch(err){
+  console.error(err);
+}
+
+// It's very important to trigger this callback method when you're finished with Bootstrap! (Otherwise your server will never lift, since it's waiting on Bootstrap)
+cb();
 
 };
